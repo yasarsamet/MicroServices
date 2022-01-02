5a0b416f2b94e9b2b86104b29cb828c9dd711ed9
Yaşar Samet Alıç <49351605+yasarsamet@users.noreply.github.com>
2022-01-02 13:56:46 +0300

    devam edildi

---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotoController.cs
@@ -0,0 +1,53 @@
+﻿using FreeCourse.Services.PhotoStock.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.ControllerBases;
+using Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : CustomBaseController
+    {
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if (photo != null && photo.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await photo.CopyToAsync(stream, cancellationToken);
+                }
+                var returnPath = "photos/" + photo.FileName;
+
+                PhotoDto photoDto = new() { Url = returnPath };
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+            }
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("photo is empty", 400));
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("photo not found", 404));
+            }
+            System.IO.File.Delete(path);
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
+    }
+}
